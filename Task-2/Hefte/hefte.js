c06d4c91df4d48866f490936c957dc6cbc1c40bf
Oruc99 <orucbaxsaliyev9@gmail.com>
2024-02-09 14:37:06 +0400

    Task-2

--- Task-2/Hefte/hefte.js
+++ Task-2/Hefte/hefte.js
@@ -24,3 +24,39 @@ else if(hefte=='7'){
 else{
     console.log('bele gun yoxdur');
 }
+
+
+
+
+
+
+
+// let hefte = +prompt('necenci gun ')
+
+// switch(hefte){
+//     case 1 :
+//         console.log('bazar ertesi');
+//         break;
+//     case 2 :
+//         console.log('cersembe axsami');
+//         break;
+//     case 3 :
+//         console.log('cersembe');
+//         break;
+//     case 4 :
+//         console.log('cuma axsami');
+//         break;
+//     case 5 :
+//         console.log('cuma');
+//         break;
+//     case 6 :
+//         console.log('sembe');
+//         break;
+//     case 7 :
+//         console.log('bazar');
+//         break;
+//     default:
+//         console.log('bele bir gun yoxdur');
+// }
+
+
